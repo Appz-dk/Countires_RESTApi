04df11d763c73d95dc8fb2349cf9dac8362bdaa1
Appz-dk <patrick.fyhn@gmail.com>
2023-10-25 22:04:14 +0200

    Added filter component

---
+++ src/components/Filter.tsx
@@ -0,0 +1,52 @@
+import { useRef, useState } from "react";
+import { ChevronDown } from "../assets/ChevronDown";
+import "./Filter.css"
+import { ChevronUp } from "../assets/ChevronUp";
+
+const filterOptions = ["Africa", "America", "Asia", "Europe", "Oceania"]
+const defaultOption = "Filter by Region"
+
+const Filter = () => {
+  const [open, setOpen] = useState(false)
+  const [filterOption, setFilterOption] = useState(defaultOption)
+  const dropdownRef = useRef<HTMLButtonElement>(null)
+
+  const handleOptionChange = (opt: string) => {
+    if (opt === filterOption) {
+      setFilterOption(defaultOption)
+    } else {
+      setFilterOption(opt)
+    }
+    setOpen(false)
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLLIElement>, opt: string) => {
+    if (e.code !== "Space") return
+    handleOptionChange(opt)
+    dropdownRef?.current?.focus()
+  }
+
+  const chevron = open ? <ChevronDown /> :<ChevronUp />
+  return (
+    <div className="filter-dropdown">
+      <button ref={dropdownRef} aria-label="filter dropdown" onClick={() => setOpen(prev => !prev)}>{filterOption} {chevron}</button>
+    {open && (
+      <ul className="filter-options">
+        {filterOptions.map(opt => (
+          <li 
+          className={filterOption === opt ? "active" : ""}
+          aria-label="filter option" 
+          onClick={() => handleOptionChange(opt)}
+          onKeyDown={e => handleKeyDown(e, opt)}
+          tabIndex={0} 
+          >
+            {opt}
+          </li>
+        ))}
+      </ul>
+    )}
+    </div>
+  );
+};
+
+export default Filter;
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,9 +1,13 @@
 import Search from "./Search";
 import "./MainContent.css"
+import Filter from "./Filter";
 
 const MainContent = () => {
   return <main className="main-content">
+    <div className="main-filter-container">
     <Search />
+    <Filter />
+    </div>
   </main>;
 };
 
