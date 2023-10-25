c6e407acc9067c2b2b4590e5763b97dcdfa68a2d
Appz-dk <patrick.fyhn@gmail.com>
2023-10-25 20:23:26 +0200

    Setup dark/light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Header from './components/Header'
 
 function App() {
+  const [theme, setTheme] = useState('dark');
 
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+  
   return (
     <>
-    <Header/>
+    <Header theme={theme} toggleTheme={toggleTheme}/>
     </>
   )
 }
